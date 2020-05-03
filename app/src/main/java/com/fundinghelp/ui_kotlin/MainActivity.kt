bf1a8b1b745e88de5a65ac75ebac432394b6ad5b
Moniruzzaman ShadhiN <shadhinemail@gmail.com>
2020-05-03 09:23:41 +0600

    button two added

--- app/src/main/java/com/fundinghelp/ui_kotlin/MainActivity.kt
+++ app/src/main/java/com/fundinghelp/ui_kotlin/MainActivity.kt
@@ -5,10 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.widget.Button
-import com.github.razir.progressbutton.attachTextChangeAnimator
-import com.github.razir.progressbutton.bindProgressButton
-import com.github.razir.progressbutton.hideProgress
-import com.github.razir.progressbutton.showProgress
+import androidx.core.content.ContextCompat
+import com.github.razir.progressbutton.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +15,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         buttonOne()
+        buttonTwo()
+    }
+
+    private fun buttonTwo() {
+        buttonProgressMixed.attachTextChangeAnimator()
+        bindProgressButton(buttonProgressMixed)
+
+        buttonProgressMixed.setOnClickListener {
+            showMixed(buttonProgressMixed)
+        }
     }
 
     private fun buttonOne() {
@@ -36,6 +44,31 @@ class MainActivity : AppCompatActivity() {
         Handler().postDelayed({
             button.isEnabled = true
             button.hideProgress(R.string.progressRight)
+        }, 3000)
+    }
+
+    private fun showMixed(button: Button) {
+        val animatedDrawable = ContextCompat.getDrawable(this, R.drawable.animated_check)!!
+        //Defined bounds are required for your drawable
+        val drawableSize = resources.getDimensionPixelSize(R.dimen.doneSize)
+        animatedDrawable.setBounds(0, 0, drawableSize, drawableSize)
+
+        button.showProgress {
+            buttonTextRes = R.string.loading
+            progressColor = Color.WHITE
+        }
+        button.isEnabled = false
+
+
+        Handler().postDelayed({
+            button.isEnabled = true
+
+            button.showDrawable(animatedDrawable) {
+                buttonTextRes = R.string.saved
+            }
+            Handler().postDelayed({
+                button.hideDrawable(R.string.mixedBehaviour)
+            }, 2000)
         }, 3000)
     }
 }
